0c0c2327400571d7532c30aa68364cae702d4875
Brian McGillion <bmg.avoin@gmail.com>
2025-11-21 20:01:30 +0400

    overlay: expose the package as an overlay for ease
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

---
+++ nix/overlay.nix
@@ -0,0 +1,15 @@
+# SPDX-FileCopyrightText: 2025 TII (SSRC) and the Ghaf contributors
+# SPDX-License-Identifier: Apache-2.0
+{
+  self,
+  ...
+}:
+{
+  flake.overlays.default = final: _prev: {
+    gp-gui =
+      let
+        systemPkgs = self.packages.${final.stdenv.hostPlatform.system} or self.packages.x86_64-linux;
+      in
+      systemPkgs.gp-gui;
+  };
+}
